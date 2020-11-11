a7d288d030e4aa76f23dd40921f8765245b467f0
David Im <im.david@yahoo.com>
2020-11-11 17:44:32 +0700

    Finish setup CoreML

--- HDogML/Controller/ViewController.swift
+++ HDogML/Controller/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         imagePicker.delegate        = self
-        imagePicker.sourceType      = .photoLibrary
+        imagePicker.sourceType      = .camera
         imagePicker.allowsEditing   = false
     }
     
@@ -27,6 +27,29 @@ class ViewController: UIViewController {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func detect(image: CIImage){
+        /// create a model object from the CoreML model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+             fatalError("Unable to find CoreML's model")
+        }
+        /// An image analysis request that uses a Core ML model to process images.
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+        }
+        /// tell the CoreML that we want to use this image
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        /// perform request
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+
 } /// End of class
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
@@ -36,8 +59,14 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            /// convert UIImage to CIImage which can be understood by CoreML engine
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Unable to convert UIImage to CIImage")
+            }
+            detect(image: ciImage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
 }
 
