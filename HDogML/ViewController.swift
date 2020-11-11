9f2d61a3bf270fe6af1e1487b039d5d37c0034d2
David Im <im.david@yahoo.com>
2020-11-11 12:00:21 +0700

    Initial Commit

---
+++ HDogML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HDogML
+//
+//  Created by David Im on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
